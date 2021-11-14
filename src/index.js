13389924c83ef82fddf55265e9176fe7abf3139b
darkeden48 <dzalex72@list.ru>
2021-11-14 16:16:19 +0200

    finish

--- src/index.js
+++ src/index.js
@@ -1,26 +1,43 @@
+// import './js/menu-js.js';
+// import './js/theme.js';
 
-import './sass/main.scss';
-import menuCards from './templates/menu-tpl.hbs';
+import menuCardTpl from './templates/menu-tpl.hbs';
 import menu from './menu.json';
 
+const menuContainers=document.querySelector('.js-menu');
+const cardsMarkup=createMenuCardMarkup(menu);
+const changeTheme=document.querySelector('#theme-switch-toggle');
+const bodySite=document.querySelector('body')
+
+menuContainers.insertAdjacentHTML("beforeend",cardsMarkup);
+
+changeTheme.addEventListener('change',themeChangeClick);
+
 const Theme = {
-  LIGHT: 'light-theme',
-  DARK: 'dark-theme',
-};
+    LIGHT: 'light-theme',
+    DARK: 'dark-theme',
+  };
 
-const bodyRef = document.querySelector('body');
-const themeSwitcherCheckboxRef = document.querySelector('.theme-switch__toggle');
-const menuCardsRef = document.querySelector('.menu');
-menuCardsRef.insertAdjacentHTML('beforeend', menuCards(menu));
-
-if (localStorage.getItem('theme') === 'dark') {
-  bodyRef.classList.toggle(`${Theme.DARK}`);
-  themeSwitcherCheckboxRef.checked = true;
+function createMenuCardMarkup(menu){
+    return menu.map(menuCardTpl).join('');
 }
 
-themeSwitcherCheckboxRef.addEventListener('change', () => {
-  bodyRef.classList.toggle(`${Theme.DARK}`);
-  themeSwitcherCheckboxRef.checked
+if (localStorage.getItem('theme') === 'dark') {
+    bodySite.classList.toggle(`${Theme.DARK}`);
+    changeTheme.checked = true;
+  }
+
+function themeChangeClick(event){
+    bodySite.classList.toggle(`${Theme.DARK}`);
+    changeTheme.checked
     ? localStorage.setItem('theme', 'dark')
     : localStorage.setItem('theme', 'light');
-});
+}
+    // if(event.currentTarget.checked){
+    // bodySite.classList.add('dark-theme');
+    // bodySite.classList.remove('light-theme');
+    // }
+    // else{
+    //     bodySite.classList.add('light-theme');
+    //     bodySite.classList.remove('dark-theme');
+    // }
---
+++ src/js/menu-js.js
@@ -0,0 +1,10 @@
+// import menuCardTpl from './templates/menu-tpl.hbs';
+// import menu from './menu.json';
+
+// const menuContainers=document.querySelector('.js-menu');
+// const cardsMarkup=createMenuCardMarkup(menu);
+// menuContainers.insertAdjacentHTML("beforeend",cardsMarkup);
+
+// function createMenuCardMarkup(menu){
+//     return menu.map(menuCardTpl).join('');
+// }
